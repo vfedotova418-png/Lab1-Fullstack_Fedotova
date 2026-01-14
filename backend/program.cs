24483e90cd2b9a8adec530480bdbaa147bfba581
vfedotova418-png <vfedotova418@gmail.com>
2026-01-14 09:18:25 +0300

    Continue menu added

--- backend/program.cs
+++ backend/program.cs
@@ -5,3 +5,11 @@ Console.Write("Введите ваше имя: ");
 string name = Console.ReadLine().ToString();
 Console.WriteLine($"Привет, {name}!");
 Console.WriteLine(DateTime.Now.DayOfWeek);
+Console.WriteLine("Хотите продолжить? (y/n)");
+string choice = Console.ReadLine().ToString();
+if (choice == "yes")
+    Console.WriteLine("Отлично, давайте продолжим.");
+else if (choice == "no")
+    Console.WriteLine("Завершение работы...");
+else
+    Console.WriteLine("Некорректный ввод.");
