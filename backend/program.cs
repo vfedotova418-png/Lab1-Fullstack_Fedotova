25fb4a8304dea440d5961da2217b2c94d5c684da
vfedotova418-png <vfedotova418@gmail.com>
2026-01-14 09:12:13 +0300

    Name input and personal greeting added

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Меня зовут Федотова В.С.");
 Console.WriteLine(DateTime.Now);
-Console.WriteLine(DateTime.Now);
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine().ToString();
+Console.WriteLine($"Привет, {name}!");
